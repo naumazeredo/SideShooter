a1ef7b6e50807af46219feefacd791e5048f3f1e
Naum Azeredo <naumazeredo@gmail.com>
2014-03-06 01:46:10 -0300

    Added particles to Jetpack

---
+++ Assets/Scripts/SetLayerScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetLayerScript : MonoBehaviour
+{
+  public string layer = "";
+
+  // Use this for initialization
+  void Start()
+  {
+    renderer.sortingLayerName = layer;
+  }
+}

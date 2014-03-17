241af1fb531b2399fe2553a3d5686121d3cefd6d
Naum Azeredo <naumazeredo@gmail.com>
2014-03-17 20:15:27 -0300

    Added Object Pooling and extension methods to Vector

---
+++ Assets/Scripts/ObjectPool/SelfPoolScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfPoolScript : MonoBehaviour
+{
+  public void PoolObject()
+  {
+    ObjectPoolScript.instance.PoolObject(gameObject);
+  }
+}
---
+++ Assets/Scripts/VectorExt.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class VectorExt
+{
+  // Vector3 extension 
+  public static Vector2 xy(this Vector3 vec)
+  {
+    return new Vector2(vec.x, vec.y);
+  }
+
+  public static Vector2 xz(this Vector3 vec)
+  {
+    return new Vector2(vec.x, vec.z);
+  }
+
+  public static Vector2 yz(this Vector3 vec)
+  {
+    return new Vector2(vec.y, vec.z);
+  }
+
+  // Vector2 extension
+  public static Vector2 invert(this Vector2 vec)
+  {
+    return new Vector2(vec.y, vec.x);
+  }
+
+  public static Vector3 V2toV3(this Vector2 vec)
+  {
+    return new Vector3(vec.x, vec.y);
+  }
+}
